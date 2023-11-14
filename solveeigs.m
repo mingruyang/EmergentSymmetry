c8fddb1a3b69846555d4254d74b40ffa90f03eab
Mingru Yang <mingruy@uci.edu>
2023-11-14 14:07:53 +0100

    Update solveeigs.m
    Add an explanation for the parity and time reversal options

--- solveeigs.m
+++ solveeigs.m
@@ -3,7 +3,7 @@ writedir = '/write path for certain bond dimension/D';
 d = 2; % dimension of 1-site physical Hilbert space
 N = 2; % number of sites G is supported from, currently only N = 1,2,3,4 have been implemented
 p = 0; % momentum
-poddteven = true; % quantum number for the parity (p) and time reversal (t) symmetry
+podd = 1; teven = 1; % quantum number for the parity (p) and time reversal (t) symmetry, could be (podd, teven)=(1,1), (podd, teven)=(0,0), (podd, teven)=(0,-1), (podd, teven)=(-1,-1), with 1=true, 0=false, -1=not impose
 D = 10; % bond dimension of MPS
 
 % read the MPS tensors which has been optimized by VUMPS
@@ -18,7 +18,7 @@ ALt = reshape(cell2mat(AL),[D,d,D]);
 ARt = reshape(cell2mat(AR),[D,d,D]);
 
 % solve the eigenvalue problem for D = 10
-AfunG = @(x) applytoGp_ncon(x, ALt, ARt, L, R, N, d, D, p, poddteven);
+AfunG = @(x) applytoGp_ncon(x, ALt, ARt, L, R, N, d, D, p, podd, teven);
 [VG, lambdaG] = eigs(AfunG,d^(2*N),d^(2*N));
 eigm = diag(lambdaG);
 format long;
@@ -48,7 +48,7 @@ for D = [20:10:100,120,160,240,400]
     save([writedir num2str(D) '/Vpoddteven_' num2str(N) 'site_0_variational_Gwhole.mat'],'VG');
 end
 
-% show the coefficients of the Pauli strings in G
+% show the coefficients of the Pauli strings in G, with '0'='I', '1'='X', '2'='Y', '3'='Z'
 k = 3; % choose the 3rd largest eigenvalue
 G = reshape(VG(:,k),[2^N,2^N]);
 op = pauliproductbasis(N);
